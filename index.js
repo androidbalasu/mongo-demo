247c2a6ecb2af30d767a9ed3d7564d1acc639922
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-22 16:17:13 -0500

    Using the OR operator.

--- index.js
+++ index.js
@@ -43,10 +43,12 @@ async function GetCourses(){
     //in
     //nin (not in)
 
+    //Logical operators
+    //or and and
+
     const courses = await Course
-                                //.find({author: 'Prash',isPublished: true})
-                                //.find({price: {$gte: 10, $lte: 20}})  
-                                .find( {price: { $in: [10, 15, 20]}})
+                                .find()
+                                .or([{author: 'Prash'}, {isPublished: true}])  //Courses published by Prash or published courses.
                                 .limit(10)  //Limit the number of results.
                                 .sort({name: 1}) //Sort in ascending order.
                                 .select({name: 1, tags: 1}); //Select the desired properties in the output.

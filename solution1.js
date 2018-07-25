6a6b9796ad4d82ceabe8ef605d85b95b56744417
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-25 13:51:01 -0500

    Update documents query first.

--- solution1.js
+++ solution1.js
@@ -1,44 +1,83 @@
-//Import mongoose module.
-//Make a connection to the database.
-//Define the schema for the collection.
-//Create an instance of the Schema and get a model to query documents in the collection.
+const mongoose = require('mongoose');   //Returns a mongoose object.  It is a library to help data modeling in mongodb.  As mongodb 
+                                        //is a nosql database, the collection has a schema and is modelled using mongoose.   Using mongoose,
+                                        //the collections can be queried, filtered, updated etc.,
 
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/mongo-exercises')
-        .then(() => console.log('Connected to mongodb instance mong-exercises'))
-        .catch(error => console.log('Error '+ error));
-
-const CourseSchema = mongoose.Schema({
-    tags: [String],
-    date: Date,
+mongoose.connect('mongodb://localhost/playground')
+        .then(()=> console.log('Connected to MongoDb...'))
+        .catch(error => console.error('Coudln\'t connect to mongodb', error));
+    
+//Define the schema for the document collection.
+const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
-    isPublished: Boolean,
-    price: Number
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
 });
 
-const Course = mongoose.model('Course', CourseSchema);
+//Create a model:  Model defines a programming interface for interacting with the databsae (CRUD) etc.,
+const Course = mongoose.model('Course', courseSchema);
+
+async function CreateCourse(){
+    //Create a course object
+    const course = new Course ({
+    name: 'Angular Course',
+    author: 'Prash',
+    tags: ['angular', 'frontend'],  //document is mongodb can be a complex object like an array.
+    isPublished: true
+    }) ;
+
+    const result = await course.save();
+    console.log(result);
+}
+//CreateCourse();
 
 async function GetCourses(){
-return await Course.find({isPublished: true})
-      .or([{price: {$gte: 15}}, {name: /.*by.*/i}])
-      .sort({price: -1})
-      .select({name: 1, author: 1, price: 1})
-}
+    const pageNumber = 2;
+    const pageSize = 10;   //Normally these are sent as query parameters to a REST api.
 
-async function run(){
-    try { 
-    const courses = await GetCourses();
+    //   /api/courses?pageNumber=2&pageSize=10
+
+    //Find an author whose name starts with 'Prash'
+    const courses = await Course
+                                .find({author: 'Prash', isPublished: true}) 
+                                .skip((pageNumber - 1) * pageSize)
+                                .limit(pageSize)  //Limit the number of results.
+                                .sort({name: 1}) //Sort in ascending order.
+                                // .select({name: 1, tags: 1}); //Select the desired properties in the output.
+                                .count();
     console.log(courses);
-    }
-    catch(error){
-        console.log('Error '+ error);
-    }
 }
 
-run();
+//GetCourses();
 
+async function updateCourse(id){
+    //Approach: Query first.
+    // findById()
+    // Modify its properties
+    //Save()
 
+    const course = await Course.findById(id);
+    if (!course) return;
 
+    //1) Setting properties individually.
+    course.isPublished = true;
+    course.author = 'Another author';
+
+    // //2) Setting properties using the set method.
+    // course.set({
+    //     isPublished: true,
+    //     author: 'Another author'
+    // });
+
+    const result = await course.save();
+
+    console.log(result);
+
+    //Approach: Update first
+    //Update directly
+    //Optionally get the updated document.
+}
+
+updateCourse('5b50fdfc7b3ba87b2c6db2f5');
 
